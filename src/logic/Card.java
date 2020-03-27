73871451a672b1557a58a0f83c08465404b13c95
Siff <siff.ravn@gmail.com>
2020-03-27 13:41:42 +0100

    (Logic) Implement Card

--- src/logic/Card.java
+++ src/logic/Card.java
@@ -1,8 +1,44 @@
 package logic;
 
 public class Card {
-    char suit;
-    boolean color;
-    int value;
+    private char suit;
+    private boolean color; //TODO better naming?
+    private int value;
     Card nextCard;
+
+    public Card(char suit, int value){
+        setSuit(suit);
+        setValue(value);
+        nextCard = null;
+    }
+
+    // Getters and Setters
+    public char getSuit() {
+        return suit;
+    }
+
+    public boolean isColor() {
+        return color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void setSuit(char suit) {
+        this.suit = suit;
+
+        if (suit == 'D' || suit == 'H') {
+            setColor(true);
+        }
+        else setColor(false);
+    }
+
+    private void setColor(boolean color) {
+        this.color = color;
+    }
+
+    private void setValue(int value) {
+        this.value = value;
+    }
 }
