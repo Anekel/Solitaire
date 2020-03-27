a8fa72b4b8f7f02e5d2b720e705e37288e439234
Siff <siff.ravn@gmail.com>
2020-03-28 00:22:28 +0100

    (Logic) Implement Solitaire base features

--- src/logic/Solitaire.java
+++ src/logic/Solitaire.java
@@ -1,25 +1,49 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Solitaire {
-    ArrayList<BuildingTower> buildingList = new ArrayList<>();
-    ArrayList<BaseStack> baseStacks = new ArrayList<>();
-    Card currentcard;
+    private ArrayList<BuildingTower> towerList = new ArrayList<>();
+    private HashMap<Character, BaseStack> baseStackMap = new HashMap<>();
+    private Card currentCard;
 
-    private  void removeFromTower(int index, Card card) {
-
+    public Solitaire(){
+        // TODO receive input data?
     }
 
-    private void moveToTower(Card card) {
+    private void removeFromTower(int index, Card card) {
+        towerList.get(index).removeCard(card);
+    }
+
+    private void moveToTower(int index, Card card) {
         //Move one or more cards from one tower to another.
+        towerList.get(index).addCard(card);
     }
 
     private void moveToBaseStack (Card card) {
         //Move a card to it's base stack
+        char suit = card.getSuit();
+        if (!baseStackMap.containsKey(suit)) baseStackMap.put(suit, new BaseStack(card));
+        else{
+            baseStackMap.get(suit).push(card);
+        }
     }
 
-    private Card drawCard() {
-        return new Card();
+    // Getters and Setters
+    public ArrayList<BuildingTower> getTowerList() {
+        return towerList;
+    }
+
+    public HashMap<Character, BaseStack> getBaseStackMap() {
+        return baseStackMap;
+    }
+
+    public Card getCurrentCard() {
+        return currentCard;
+    }
+
+    public void setCurrentCard(Card card) {
+        this.currentCard = card;
     }
 }
