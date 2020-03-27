149255fc1ada40a1190f0aa0d2f40b83390fa2d9
Siff <siff.ravn@gmail.com>
2020-03-27 13:56:13 +0100

    (Logic) Implement BaseStack

--- src/logic/BaseStack.java
+++ src/logic/BaseStack.java
@@ -3,6 +3,29 @@ package logic;
 import java.util.Stack;
 
 public class BaseStack {
-    char suit;
-    Stack<Card> cards = new Stack<>();
+    private char suit;
+    private Stack<Card> cards = new Stack<>();
+
+    public BaseStack(Card ace){
+        setSuit(ace.getSuit());
+        push(ace);
+    }
+
+    public void push(Card card){
+        cards.push(card);
+    }
+
+    public Card pop(){
+        return cards.pop();
+    }
+
+    // Getters and Setters
+    public char getSuit() {
+        return suit;
+    }
+
+    public void setSuit(char suit) {
+        this.suit = suit;
+    }
+
 }
