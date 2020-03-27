e1da147846c401a9e04cf385c13a1c7b1bf9f704
Siff <siff.ravn@gmail.com>
2020-03-27 14:23:05 +0100

    (Logic) Implement BuildingTower

--- src/logic/BuildingTower.java
+++ src/logic/BuildingTower.java
@@ -3,15 +3,43 @@ package logic;
 import java.util.ArrayList;
 
 public class BuildingTower {
-    int faceDown;
-    Card head;
-    Card  end;
+    private int faceDown;
+    private Card head;
+    private Card  end;
+
+    public BuildingTower(int index, Card head){
+        setFaceDown(index);
+        setHead(head);
+    }
 
     private Boolean isEmpty() {
-        if (faceDown == 0) {
-            return true;
-        }
-        return false;
+        return faceDown == 0 && head == null;
+    }
+
+    // Getters and Setters
+    public int getFaceDown() {
+        return faceDown;
+    }
+
+    public Card getHead() {
+        return head;
+    }
+
+    public Card getEnd() {
+        return end;
+    }
+
+    public void setFaceDown(int faceDown) {
+        this.faceDown = faceDown;
+    }
+
+    public void setHead(Card head) {
+        this.head = head;
+        if (head.nextCard == null) setEnd(head);
+    }
+
+    public void setEnd(Card end) {
+        this.end = end;
     }
 }
 
