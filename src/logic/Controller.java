0de93ebc5bcde5190dc997ed4f4d4b95c410e7c6
Siff <siff.ravn@gmail.com>
2020-04-15 10:20:28 +0200

    (Controller) Implement flow

--- src/logic/Controller.java
+++ src/logic/Controller.java
@@ -1,8 +1,17 @@
 package logic;
 
-public class Controller {
-    Solitaire game = new Solitaire();
-    public static void main(String[] args) {
+import algorithm.Algorithm;
 
+public class Controller {
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        Solitaire game = new Solitaire(deck);
+        System.out.println(game.toString());
+
+        Algorithm.ALG(game);
+
+        System.out.println(game.toString());
     }
 }
