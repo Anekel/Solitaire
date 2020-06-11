0ca55a830b90e504d3ca12cfad7d2f9ede81fd5a
Siff <siff.ravn@gmail.com>
2020-06-11 15:14:13 +0200

    Feat (StateFactory) Implement method headers

--- src/main/java/logic/SolitaireStateFactory.java
+++ src/main/java/logic/SolitaireStateFactory.java
@@ -1,4 +1,29 @@
 package logic;
 
+import DTO.SolitaireDTO;
+
 public class SolitaireStateFactory {
+
+    private Solitaire game = null;
+
+    public void updateGame(SolitaireDTO DTO){
+        if (game == null){
+            game = new Solitaire(DTO);
+        }
+        else{
+            game = changes(DTO);
+        }
+    }
+
+    private Solitaire changes(SolitaireDTO DTO){
+        return null;
+    }
+
+    public Solitaire getGame() {
+        return game;
+    }
+
+    public void setGame(Solitaire game) {
+        this.game = game;
+    }
 }
