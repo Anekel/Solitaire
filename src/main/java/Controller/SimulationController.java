01a40bae1f2c93fb39f867ffda6e03e9d0474791
Siff <siff.ravn@gmail.com>
2020-06-12 16:07:15 +0200

    Feat (StateFactory) Apply StateFactory in SimulationController

--- src/main/java/Controller/SimulationController.java
+++ src/main/java/Controller/SimulationController.java
@@ -5,6 +5,7 @@ import algorithm.ReturnData;
 import logic.Card;
 import logic.Deck;
 import logic.Solitaire;
+import logic.SolitaireStateFactory;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -12,14 +13,12 @@ import java.util.Scanner;
 public class SimulationController {
 
     private static ArrayList<Card> movedKings = new ArrayList<>();
+    private static SolitaireStateFactory stateFactory = new SolitaireStateFactory();
+    private static Deck deck = new Deck();
 
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.shuffle();
-        Solitaire game = new Solitaire(deck);
 
-        game.setCurrentCard(deck.draw());
-        System.out.println(game.toString());
+        init();
 
         Scanner scan = new Scanner(System.in);
         String str = "";
@@ -27,22 +26,36 @@ public class SimulationController {
         while (!str.equals("X")){
             boolean noMoves = false;
             while (!noMoves){
+                Solitaire game = stateFactory.getGame();
+
                 ReturnData returnData = Algorithm.ALG(game, movedKings);
                 noMoves = returnData.isNoMoves();
                 movedKings = returnData.getMovedKings();
                 System.out.println(game.toString());
                 if(noMoves) continue;
 
+                stateFactory.updateGameFromGame(game);
+
                 str = scan.next();
             }
 
+            Solitaire game = stateFactory.getGame();
             if(game.getCurrentCard() != null){
                 deck.discard(game.getCurrentCard());
             }
             game.setCurrentCard(deck.draw());
             System.out.println("\n" + "Card drawn!");
             System.out.println(game.toString());
+            stateFactory.setGame(game);
             str = scan.next();
         }
+    }
+
+    private static void init(){
+        deck.shuffle();
+        Solitaire game = new Solitaire(deck);
+        game.setCurrentCard(deck.draw());
+        stateFactory.updateGameFromGame(game);
+        System.out.println(game.toString());
     }
 }
