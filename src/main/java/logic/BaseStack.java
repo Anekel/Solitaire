b42305bd015f391b512046fb6d4ae44b7960fd89
Siff <siff.ravn@gmail.com>
2020-06-18 09:53:34 +0200

    Minor fix: Remove suit from baseStack as it is redundant

--- src/main/java/logic/BaseStack.java
+++ src/main/java/logic/BaseStack.java
@@ -3,11 +3,9 @@ package logic;
 import java.util.Stack;
 
 public class BaseStack {
-    private char suit;
     private Stack<Card> cards = new Stack<>();
 
     public BaseStack(Card ace){
-        setSuit(ace.getSuit());
         push(ace);
     }
 
@@ -17,15 +15,6 @@ public class BaseStack {
 
     public Card pop(){
         return cards.pop();
-    }
-
-    // Getters and Setters
-    public char getSuit() {
-        return suit;
-    }
-
-    public void setSuit(char suit) {
-        this.suit = suit;
     }
 
     public String toString(){
